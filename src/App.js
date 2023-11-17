73b52dca93ab71b5370abe83d1c401e5162e6178
YoungCheon Kim <kyt031522@gmail.com>
2023-11-17 19:40:59 +0900

    .

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import {
   Routes,
   Route,
 } from  "react-router-dom";
-import LoginMain from "./component/User/Login_Main"
-import Login from "./component/User/Login/User_Login"
-import RegisterTel from "./component/User/Register/Register_Tel"
-import RegisterNickname from "./component/User/Register/Register_Nickname"
-import RegisterCrew from "./component/User/Register/Register_Crew"
+
 
 import Main from "./component/Main/Main";
 import styled from "styled-components"
@@ -17,17 +13,9 @@ import Navbar from "./component/Navbar/Navbar"
 
 import Clothes from "./component/Shoes/Shoes_Main/Shoes_Main";
 import { Outlet } from 'react-router-dom';
-import ShoesSearch from "./component/Shoes/Shoes_Search/Shoes_Search"
+
 import Shoes_Detail from "./component/Shoes/Shoes_Detail/Shoes_Detail"
-import ShoesAdd from "./component/Shoes/Shoes_Add/Shoes_Add"
 
-import Crew from "./component/Crew/Crew_Main/Crew_Main"
-import CrewLocation from "./component/Crew/Crew_Location/Crew_Location"
-import CrewDetail from './component/Crew/Crew_Detail/Crew_Detail';
-import CrewSearch from "./component/Crew/Crew_Search/Crew_Search"
-import CrewAdd from "./component/Crew/Crew_Add/Crew_Add"
-
-import { motion, AnimatePresence } from 'framer-motion';
 import {
   RecoilRoot,
   atom,
@@ -63,40 +51,12 @@ function App() {
               <Route element={<WithNav/>}>
                 <Route path ="/" element={<Main/>}/>
                 <Route path="/clothes" element={<Clothes/>}/>
-                {/* <Route path='/schedule' element = {<Schedule/>}/>
-                <Route path='/schedule/search' element ={<ScheduleSearch/>}/>
 
-                <Route path='/shoes' element ={<Shoes/>}/>
-                <Route path='/shoes/search' element ={<ShoesSearch/>}/>
-
-                <Route path='/crew' element ={<Crew/>}/>
-                <Route path="/crew/location" element={<CrewLocation/>}/>
-                <Route path="/crew/detail/:id" element={<CrewDetail/>}/>
-                <Route path='/crew/search' element ={<CrewSearch/>}/>
-
-                <Route path="/runnertalk" element={<RunnerTalk/>}/>
-                <Route path="/runnertalk/search" element={<RunnerTalkSearch/>}/> */}
-                
               </Route>
 
               <Route element={<WithoutNav/>}>
               <Route path="/clothes/detail/:id" element={<Shoes_Detail/>}/> 
-                {/* <Route path="/login/main" element={<LoginMain/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/register/tel" element={<RegisterTel/>}/>
-                <Route path="/register/nickname" element={<RegisterNickname/>} />
-                <Route path="/register/crew" element={<RegisterCrew/>} />
 
-                <Route path='/competition/detail/:id' element ={<Competition_Detail/>}/>  
-                <Route path='/competition/add' element ={<CompetitionAdd/>}/>
-
-                <Route path="/shoes/add" element={<ShoesAdd/>}/>
-
-                <Route path="/runnertalk/category/:id" element={<RunnerTalkFilter/>}/>
-                <Route path="/runnertalk/detail/:id" element={<RunnerTalkDetail/>}/>
-                <Route path="/runnertalk/write" element={<RunnerTalkWrite/>}/>
-
-                <Route path="/crew/add" element={<CrewAdd/>}/> */}
               </Route>
 
             </Routes>
--- src/component/Main/TopBar/TopBar.js
+++ src/component/Main/TopBar/TopBar.js
@@ -3,11 +3,6 @@ import React, { useState } from "react";
 import { useRef,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-import InputBase from '@mui/material/InputBase';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import WestIcon from '@mui/icons-material/West';
-import Logo from "../../../Image/White_Logo.png"
 import { API_URL } from '../../../API/URL';
 
 export default function Main_TopBar(){
@@ -39,7 +34,7 @@ export default function Main_TopBar(){
             position:'fixed',
             top:10,
             display:'flex',
-            justifyContent:'space-between',
+            justifyContent:'end',
             alignItems:'center',
             height:'60px',
             width:'100%',
@@ -50,9 +45,7 @@ export default function Main_TopBar(){
             backgroundColor:'transparent',
             borderBottom:0,
         }}>
-            <Box onClick = {navigateToScheduleMain} sx={{display:'flex',justifyContent:'center',alignItems:'center',ml:2}}>
-                <Box component='img' src={Logo} sx={{}}/>
-            </Box>
+
 
             <Box sx={{mr:2}}>
                 <Avatar alt="Profile" src={`${API_URL}${profile}`} />
